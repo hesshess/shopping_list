3cc72d4c0adbf0c9473ee800c6de89e0a47d0d11
hesshess <64644262+hesshess@users.noreply.github.com>
2020-08-28 13:25:45 +0100

    final_event_update

--- main.js
+++ main.js
@@ -3,38 +3,45 @@ const input = document.querySelector('.footer__input')
 const addBtn = document.querySelector('.footer__button')
 
 function onAdd () {
-  //1. getting the text that user wrote
   const text = input.value
   if (text === '') {
     input.focus()
     return
   }
-  // 2. making a new item (text + delete Btn)
+
   const item = createItem(text)
-  // 3. adding the new item to the items container
   items.appendChild(item)
-  //   4. scrolling to the new item
   item.scrollIntoView({ block: 'center' })
-  // 5. initialize the input
   input.value = ''
   input.focus()
 }
 
-let id = 0 //UUID
-
 function createItem (text) {
   const itemRow = document.createElement('li')
   itemRow.setAttribute('class', 'item__row')
-  itemRow.setAttribute('data-id', id)
-  itemRow.innerHTML = `
-      <div class="item">
-          <span class="item__name">${text}</span>
-          <button class="item__delete" >
-          <i class="fas fa-trash-alt" data-id=${id}></i></button>
-      </div>
-      <div class="item__divider"></div>
-    `
-  id++
+
+  const item = document.createElement('div')
+  item.setAttribute('class', 'item')
+
+  const name = document.createElement('span')
+  name.setAttribute('class', 'item__name')
+  name.innerText = text
+
+  const deleteBtn = document.createElement('button')
+  deleteBtn.setAttribute('class', 'item__delete')
+  deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>'
+  deleteBtn.addEventListener('click', () => {
+    itemRow.remove()
+  })
+
+  const itemDivider = document.createElement('div')
+  itemDivider.setAttribute('class', 'item__divider')
+
+  item.appendChild(name)
+  item.appendChild(deleteBtn)
+
+  itemRow.appendChild(item)
+  itemRow.appendChild(itemDivider)
   return itemRow
 }
 
@@ -45,14 +52,5 @@ addBtn.addEventListener('click', () => {
 input.addEventListener('keypress', event => {
   if (event.key === 'Enter') {
     onAdd()
-  }
-})
-
-items.addEventListener('click', event => {
-  const id = event.target.dataset.id
-
-  if (id) {
-    const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`)
-    toBeDeleted.remove()
   }
 })
